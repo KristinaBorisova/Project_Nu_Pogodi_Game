fa49b6ce586e230d098293ad2eb209a777ae2fa6
DESKTOP-FCAF4EF\USER <kkristinaborisova@gmail.com>
2017-03-20 21:53:43 +0200

    making some different tasks

--- src/MainUserScreen.java
+++ src/MainUserScreen.java
@@ -1,16 +1,21 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainUserScreen {
 
 	private JFrame frame;
+	private JButton btnRegister;
+	private JButton btnPlayGame;
+	private JButton btnLogIn;
 
-	/**
-	 * Launch the application.
-	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -24,33 +29,75 @@ public class MainUserScreen {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+	
 	public MainUserScreen() {
 		initializeElements();
+//		buttonsActinos();
+	}
+	
+
+	
+//		btnPlayGame.addActionListener(new ActionListener() { // PlayGame Button sends us to PlayGameClass
+//			public void actionPerformed(ActionEvent e) {
+//				
+//			}
+//		});
+//
+//		
+//		
+//		btnLogIn.addActionListener(new ActionListener() { //LogIn Button opens Login Form
+//			public void actionPerformed(ActionEvent e) {
+//			}
+//		});
+//		
+		
+	
+	
+
+	/*private void buttonsActinos() {
+		
+		btnRegister.addActionListener(new ActionListener() { //Register button opens RegistrationForm Class
+			public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(null, "emiii");
+			}
+		});
+		
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	*/
 	private void initializeElements() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frame.setBounds(100, 100, 650, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		JButton btnPlayGame = new JButton("Play Game");
-		btnPlayGame.setFont(new Font("Trajan Pro", Font.BOLD, 18));
-		btnPlayGame.setBounds(131, 97, 186, 51);
+		btnPlayGame.setFont(new Font("Trajan Pro", Font.BOLD, 21));
+		btnPlayGame.setBounds(197, 163, 227, 60);
 		frame.getContentPane().add(btnPlayGame);
 
 		JButton btnLogIn = new JButton("Log in");
-		btnLogIn.setBounds(180, 161, 115, 25);
+		btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnLogIn.setBounds(253, 236, 128, 43);
 		frame.getContentPane().add(btnLogIn);
 
 		JButton btnRegister = new JButton("Register");
-		btnRegister.setBounds(180, 190, 115, 25);
+		btnRegister.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnRegister.setBounds(253, 292, 128, 40);
 		frame.getContentPane().add(btnRegister);
 	}
 
--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -7,24 +7,27 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.channels.ShutdownChannelGroupException;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
-public class RegistrationForm {
+public class RegistrationForm  {
 
-	private JFrame frame;
+	JFrame frame;
 	private JTextField textfieldUsername;
 	private JTextField textFieldPassword;
 	private JButton btnRegister;
-
-	
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					RegistrationForm window = new RegistrationForm();
-					window.frame.setVisible(true);
+					RegistrationForm window2 = new RegistrationForm();
+					window2.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -32,29 +35,57 @@ public class RegistrationForm {
 		});
 	}
 
-	
 	public RegistrationForm() {
 		initializeElements();
 		register();
-		checkForExistingRegistration();
 	}
-
-	
-	private void checkForExistingRegistration() { 
-		
-		
-	}
-
 
 	private void register() {
 		String usernameInput = textfieldUsername.getText();
 		String passwordInput = textFieldPassword.getText();
+
+		String[] checkUsername = usernameInput.split(" ");
+		String[] checkPassword = passwordInput.split(" ");
 		
-		
+		if (checkUsername.length < 1 || checkUsername.length == 0) {
+
+			if (checkUsername.length == 0 || checkPassword.length == 0) {
+				JOptionPane.showMessageDialog(null, "Incorrect username or password.Try again!");
+			} else {
+				btnRegister.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+	//					saveUsernameInDatabase(passwordInput);
+						savePasswordInDatabase();
+						JOptionPane.showMessageDialog(null, "Successfull Registration!");
+					}
+				});
+			
+
+			}
+		}
 	}
-		
 
+	private void savePasswordInDatabase() {
+	
 
+	}
+
+//	private void saveUsernameInDatabase(String username) {
+//		CheckForExistingAccount();
+//		try (FileWriter fw = new FileWriter("C:\\Users\\USER\\Desktop\\Database\\Username.txt", true);
+//				BufferedWriter bw = new BufferedWriter(fw);
+//				PrintWriter out = new PrintWriter(bw)) {
+//			out.println(username);
+//		} catch (IOException e) {
+//			System.out.println(e);
+//		}
+//	}
+
+	private void CheckForExistingAccount() { // If Username == username in
+												// database, Choose another
+		// TODO Auto-generated method stub
+
+	}
 
 	private void initializeElements() {
 		frame = new JFrame();
@@ -63,25 +94,29 @@ public class RegistrationForm {
 		frame.getContentPane().setLayout(null);
 
 		JButton btnRegister = new JButton("Register");
-		btnRegister.setBounds(124, 167, 211, 40);
+		btnRegister.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnRegister.setBounds(133, 189, 211, 40);
 		frame.getContentPane().add(btnRegister);
 
 		textfieldUsername = new JTextField();
-		textfieldUsername.setBounds(101, 22, 278, 49);
+		textfieldUsername.setBounds(133, 41, 266, 40);
 		frame.getContentPane().add(textfieldUsername);
 		textfieldUsername.setColumns(10);
 
 		textFieldPassword = new JTextField();
-		textFieldPassword.setBounds(101, 90, 278, 49);
+		textFieldPassword.setBounds(133, 94, 266, 40);
 		frame.getContentPane().add(textFieldPassword);
 		textFieldPassword.setColumns(10);
 
 		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setBounds(12, 41, 89, 21);
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblUsername.setBounds(26, 51, 89, 21);
 		frame.getContentPane().add(lblUsername);
 
 		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setBounds(12, 104, 89, 21);
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPassword.setBounds(26, 104, 89, 21);
 		frame.getContentPane().add(lblPassword);
 	}
+
 }
